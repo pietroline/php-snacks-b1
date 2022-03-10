c960b2fe5fd09eb36bf6122fdc9ae7c2ff9970cf
pietroline <pietrobarberi@hotmail.it>
2022-03-10 16:36:45 +0100

    snack 5

---
+++ snack5/index.php
@@ -0,0 +1,42 @@
+<!-- Prendere un testo abbastanza lungo, contenente diverse frasi. 
+Prendere il testo e suddividerlo in tanti paragrafi ogni volta in cui si incontrerà un punto. -->
+
+<?php
+
+    $text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.";
+
+    $array_frasi = explode(".", $text);
+
+    $gruppo_frasi_singole = "";
+    for($i=0; $i<count($array_frasi)-1; $i++){
+        $gruppo_frasi_singole .= ($i+1). ") ". $array_frasi[$i]. "<br>";
+    }
+    
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snack 5</title>
+    </head>
+
+    <body>
+        
+        <p>
+            <b>Paragrafo: </b>
+            <?php echo $text ?>
+        </p>
+
+        <section>
+            <h4>Frasi singole: </h4>
+            <?php echo $gruppo_frasi_singole ?>
+        </section>
+    </body>
+
+</html>
