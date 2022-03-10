2c2a8a8f26ff01053f5dc97944779a699a1e7bc1
pietroline <pietrobarberi@hotmail.it>
2022-03-10 16:36:37 +0100

    snack 2

--- snack2/index.php
+++ snack2/index.php
@@ -60,7 +60,7 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Snack 2</title>
     </head>
 
     <body>
